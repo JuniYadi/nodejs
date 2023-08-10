16b7fcc7b754266bcad0aac64630c93f64f5781e
JuniYadi <juniyadi94@gmail.com>
2023-08-11 01:06:29 +0700

    Init Class for Database

--- packages/dynamodb/src/index.ts
+++ packages/dynamodb/src/index.ts
@@ -1,13 +1,39 @@
-import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+interface DatabaseOptions {
+  table: string;
+  region: string;
+  endpoint?: string;
+  accessKeyId?: string;
+  secretAccessKey?: string;
+}
 
-const client = new DynamoDBClient({});
+export class Database {
+  public options: DatabaseOptions = {} as DatabaseOptions;
 
-export const main = async () => {
-  const command = new ListTablesCommand({});
+  constructor(options: DatabaseOptions) {
+    // init table name
+    if (options.table) {
+      this.options.table = options.table;
+    } else if (!options.table && process.env.DYNAMODB_TABLE) {
+      this.options.table = process.env.DYNAMODB_TABLE;
+    } else {
+      throw new Error("No table name provided");
+    }
 
-  const response = await client.send(command);
+    // init region
+    if (options.region) {
+      this.options.region = options.region;
+    } else if (!options.region && process.env.AWS_REGION) {
+      this.options.region = process.env.AWS_REGION;
+    } else {
+      throw new Error("No region provided");
+    }
+  }
 
-  console.log("Success", response.TableNames);
+  public async get(id: string) {
+    return {};
+  }
 
-  return response;
-};
+  public async query() {
+    return {};
+  }
+}
