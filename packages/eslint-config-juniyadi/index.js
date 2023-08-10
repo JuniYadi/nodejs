df72fb5b48a99e0354bb5fa349fa9faf5b0bb754
JuniYadi <juniyadi94@gmail.com>
2023-08-11 03:35:29 +0700

    Update Lint Error

--- packages/eslint-config-juniyadi/index.js
+++ packages/eslint-config-juniyadi/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier", "next/core-web-vitals"],
+  extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
   },
-  // parserOptions: {
-  //   babelOptions: {
-  //     presets: [require.resolve("next/babel")],
-  //   },
-  // },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
 };
