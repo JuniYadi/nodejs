34a6e36ae7ab8bf2718287750c1a98269d5fa15a
JuniYadi <juniyadi94@gmail.com>
2023-08-12 05:19:04 +0700

    Added Cognito Package

---
+++ packages/cognito/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+import type { Options } from "tsup";
+
+const env = process.env.NODE_ENV || "development";
+
+export default defineConfig({
+  entry: ["src/**/*.ts", "!src/tests/", "!src/**/*.test.ts"],
+  format: ["cjs", "esm"],
+  splitting: false,
+  sourcemap: env === "development",
+  clean: true,
+  dts: true,
+  minify: env === "production",
+  bundle: env === "production",
+  skipNodeModulesBundle: true,
+});
