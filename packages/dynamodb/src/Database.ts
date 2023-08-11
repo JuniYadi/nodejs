116a90a49911a95aacaee27adf85c1d1b5c37d90
Juni Yadi <31454084+JuniYadi@users.noreply.github.com>
2023-08-12 00:49:03 +0700

    Dynamodb database class (#6)
    * Update Package
    
    * Update DynamoDB - Decorator
    
    * Update DynamoDB - Decorator Tests
    
    * Update DynamoDB - Delete Global Types
    
    * Update DynamoDB - Database Files
    
    * Update Tsconfig - Allow Decorator
    
    * Update Turbo Config
    
    * Update DynamoDB - tsup Config and Build
    
    * Added Changeset
    
    * Update tsconfig
    
    * Update DynamoDB - Tsconfig
    
    * Update Tests
    
    * Update PR Check

---
+++ packages/dynamodb/src/Database.ts
@@ -0,0 +1,104 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { String } from "./decorator/String";
+
+interface DatabaseOptions {
+  tableName: string;
+  region: string;
+  endpoint?: string;
+}
+
+export class Database {
+  static options: DatabaseOptions = {} as DatabaseOptions;
+  public client: DynamoDBClient = {} as DynamoDBClient;
+
+  pk: string = "";
+
+  /**
+   * This is the list containing the where clauses
+   * @type {string[]}
+   * @memberof Database
+   * @static
+   * @example
+   * Database.where("id = 1");
+   * Database.where("name = 'John'");
+   */
+  static whereData: string[] = [];
+
+  /**
+   * This is the limit clause
+   * @type {number}
+   * @memberof Database
+   * @static
+   * @example
+   * Database.limit(10);
+   * Database.limit(20);
+   */
+  static limitData: number = 15;
+
+  @String()
+  private sk: string;
+
+  constructor(options: DatabaseOptions) {
+    // init table name
+    if (options.tableName) {
+      Database.options.tableName = options.tableName;
+    } else if (!options.tableName && process.env.DYNAMODB_TABLE) {
+      Database.options.tableName = process.env.DYNAMODB_TABLE;
+    } else {
+      throw new Error("No table name provided");
+    }
+
+    // init region
+    if (options.region) {
+      Database.options.region = options.region;
+    } else if (!options.region && process.env.AWS_REGION) {
+      Database.options.region = process.env.AWS_REGION;
+    } else {
+      throw new Error("No region provided");
+    }
+
+    // init dynamodb client
+    this.client = new DynamoDBClient({
+      region: Database.options.region,
+      endpoint: options.endpoint || undefined,
+    });
+
+    this.sk = "data";
+  }
+
+  public static where(val: string) {
+    this.whereData.push(val);
+  }
+
+  public static limit(val: number) {
+    this.limitData = val;
+  }
+
+  setPk(val: string) {
+    this.pk = val;
+  }
+
+  getPk() {
+    return this.pk;
+  }
+
+  public static async get(id: string) {
+    return {};
+  }
+
+  public static async query() {
+    const commands = new QueryCommand({
+      TableName: this.options.tableName,
+      KeyConditionExpression: ":pk = #pk",
+      ExpressionAttributeNames: {
+        ":pk": "pk",
+      },
+      ExpressionAttributeValues: {
+        "#pk": "pk",
+      },
+      Limit: Database.limitData,
+      ConsistentRead: true,
+    });
+  }
+}
