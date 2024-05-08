6cd36a32fa268ed2bf8861c5544d6473b97eeb72
Juni Yadi <juniyadi94@gmail.com>
2024-05-08 21:56:31 +0700

    chore: Refactor timezone handling in date functions

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -9,7 +9,7 @@ dayjs.extend(timezone);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
 
-const TIMEZONE = process.env.TIMEZONE || "Asia/Jakarta";
+const TIMEZONE = process.env.TIMEZONE;
 
 interface IDayjs {
   timezone?: string;
@@ -20,24 +20,58 @@ interface IDayjs {
   format?: string;
 }
 
+/**
+ * Extend dayjs instance
+ */
 export const date = dayjs;
 
+/**
+ * Get current date with timezone and format
+ *
+ * @param config
+ * @returns string
+ */
 export const now = (config?: IDayjs) =>
   dayjs()
-    .tz(config?.timezone)
+    .tz(TIMEZONE || config?.timezone)
     .format(config?.format || "YYYY-MM-DDTHH:mm:ssZ");
 
+/**
+ * Display date difference from now
+ *
+ * @param date string
+ * @returns string
+ */
 export const fromNow = (date: string) => dayjs(date).fromNow();
 
+/**
+ * Display date difference from now with specific timezone and format
+ *
+ * @param date string
+ * @param config IDayjs
+ * @returns string => "Sunday, 01 Jan 2021 00:00:00 +0700"
+ */
 export const fromDate = (date: string | Date, config?: IDayjs) =>
   dayjs(date)
     .tz(config?.timezone)
     .format(config?.format || "dddd, DD MMM YYYY HH:mm:ss Z");
 
+/**
+ * Display date difference from now with specific for human
+ *
+ * @param date string
+ * @param config IDayjs
+ * @returns string => "1 year ago"
+ */
 export const fromDateHuman = (date: string | Date, config?: IDayjs) =>
-  dayjs()
-    .tz(config?.timezone)
-    .to(dayjs(date));
+  dayjs().tz(config?.timezone).to(dayjs(date));
 
+/**
+ * Convert Timestamp to Date ISO String
+ *
+ * @param timestamp number
+ * @param config IDayjs
+ * @returns string => "2021-01-01T00:00:00Z"
+ */
 export const timestampToDate = (timestamp: number, config?: IDayjs) =>
   dayjs.unix(timestamp).tz(config?.timezone).toISOString();
